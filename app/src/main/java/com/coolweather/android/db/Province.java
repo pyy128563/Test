0d060fe8f6f4319d9c0a93c05ef57664fcc98e9e
pyy128563 <pyy128563@github.com>
2020-06-11 08:35:30 +0000

    Add fragment.

---
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -0,0 +1,28 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int procinceCode;
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getProvinceName(){
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName){
+        this.provinceName=provinceName;
+    }
+    public int getProcinceCode(){
+        return procinceCode;
+    }
+    public void setProcinceCode(int procinceCode){
+        this.procinceCode=procinceCode;
+    }
+}
